c890a84be502c4bb383866ff7738fbb3c8e88cdd
Wild266 <32823036+Wild266@users.noreply.github.com>
2020-02-02 17:54:02 -0500

    Update Robot2TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2TeleOp.java
@@ -61,7 +61,7 @@
      * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
      * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
      */
-
+    Webcam webcam = new Webcam.get("Webcam1");
     @TeleOp(name="Robot2TeleOp", group="Linear OpMode")
     //@Disabled
     public class Robot2TeleOp extends LinearOpMode {
